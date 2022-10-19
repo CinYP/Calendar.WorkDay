ee3bda880a6edcaa69bc4a56451a431b74701a9f
CinYP <111353883+CinYP@users.noreply.github.com>
2022-10-18 21:21:04 -0400

    added function to update time over the course of the day

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,55 @@
 let date = moment();
 $("#currentDay").text(date.format("dddd,MMMM Do"));
 
+//Preping the page to run as soon as it's safe to run JS
+$(document).ready(function(){
 //Event listener for Save Button
-console.log(document);
+$(".saveBtn").on("click", function(){
+    let text = $(this).siblings("task").val();
+    let time = $(this).parent().attr("id");
+
+    //setting local storage 
+    localStorage.setItem(time,text);
+    
+})
+}); 
+
+//Function to update based on time of day 
+function timeblocking() {
+    let currentTime = moment().hour();
+
+    //console.log(currentTime)
+
+    $(".task-info").each(function(){
+        let blockTime = parseInt($(this).attr("id"))
+        console.log(blocktime)
+        if(blockTime < currentTime){
+            $(this).removeClass("future")
+            $(this).removeClass("present")
+            $(this).addClass("past")
+        } else if (blockTime === currentTime){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+};
+
+//Getting any set tasks from local storage
+$("#9 .task").val(localstorage.getItem("9"));
+$("#10 .task").val(localstorage.getItem("10"));
+$("#11 .task").val(localstorage.getItem("11"));
+$("#12 .task").val(localstorage.getItem("12"));
+$("#13 .task").val(localstorage.getItem("13"));
+$("#14 .task").val(localstorage.getItem("14"));
+$("#15 .task").val(localstorage.getItem("15"));
+$("#16 .task").val(localstorage.getItem("16"));
+$("#17 .task").val(localstorage.getItem("17"));
+$("#18 .task").val(localstorage.getItem("18"));
+
+//declaring timeblocking function
+timeblocking();
